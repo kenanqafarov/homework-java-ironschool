bdc43b4cd2f70bbe0e790150c8b3c95fa3301b5d
kenanqafarov <qafarovkenan06@gmail.com>
2026-03-11 00:24:07 +0400

    All done

--- src/main/java/com/example/demo/Exceptions/CourseNotFoundException.java
+++ src/main/java/com/example/demo/Exceptions/CourseNotFoundException.java
@@ -1,5 +1,5 @@
 package com.example.demo.Exceptions;
-// 
+
 public class CourseNotFoundException extends RuntimeException {
     public CourseNotFoundException(String message) {
         super(message);
