4276c689c8bee9a5b5320df8871bd10eb13c3138
kenanqafarov <qafarovkenan06@gmail.com>
2026-03-11 00:18:10 +0400

    All done

--- src/main/java/com/example/demo/Exceptions/CourseNotFoundException.java
+++ src/main/java/com/example/demo/Exceptions/CourseNotFoundException.java
@@ -1,5 +1,4 @@
 package com.example.demo.Exceptions;
-// dsgsd
 public class CourseNotFoundException extends RuntimeException {
     public CourseNotFoundException(String message) {
         super(message);
